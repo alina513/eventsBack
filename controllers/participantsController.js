050d6bbdf6c197683de2dc1ca8de2c90febae129
alina513 <130853290+alina513@users.noreply.github.com>
2024-05-18 00:55:01 +0300

    Update participantsController.js

--- controllers/participantsController.js
+++ controllers/participantsController.js
@@ -2,19 +2,18 @@ const {Participant} = require("../models/participants.js");
 const HttpError = require("../helpers/HttpError.js");
 const ctrlWrapper = require("../helpers/ctrlWrapper.js");
 
-// const getAllParticipants = async (req, res) => {
-//   // const {event_id: event} = req.body;
-//   const result = await Participant.find({});
-//   res.json(result);
-// };
-
 const getAllParticipants = async (req, res) => {
-  const {id} = req.body;
-  const result = await Participant.find({
-event_id: id
-  });
+  const result = await Participant.find({});
   res.json(result);
 };
+
+// const getAllParticipants = async (req, res) => {
+//   const {id} = req.body;
+//   const result = await Participant.find({
+// event_id: id
+//   });
+//   res.json(result);
+// };
 
 
 const createParticipant = async (req, res) => {
